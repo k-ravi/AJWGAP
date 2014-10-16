b0ad932e2ebb9c601b195abfa24026f8a642e0be
k-ravi <ravinder.kandikatla@ntcsoftech.com>
2014-10-16 11:01:07 +0530

    Bulild 10-16-14

---
+++ AJWGAP/WebForm1.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="AJWGAP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <asp:GridView ID="grdView" runat="server" AllowPaging="True" PageSize="10" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px" CellPadding="3" GridLines="Horizontal" OnPageIndexChanging="grdView_PageIndexChanging">
+                <AlternatingRowStyle BackColor="#F7F7F7" />
+                <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+                <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+                <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+                <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <SortedAscendingCellStyle BackColor="#F4F4FD" />
+                <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+                <SortedDescendingCellStyle BackColor="#D8D8F0" />
+                <SortedDescendingHeaderStyle BackColor="#3E3277" />
+                              <Columns>
+                                  <asp:TemplateField HeaderText="Employee ID">
+                                      <ItemTemplate>
+                                          <asp:Label Text='<%: Eval("EmployeeId") %>' runat="server" />
+                                      </ItemTemplate>
+                                  </asp:TemplateField>
+                              </Columns>
+            </asp:GridView>
+            <br />
+            <br />
+         
+        </div>
+    </form>
+</body>
+</html>
---
+++ AJWGAP/WebForm1.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace AJWGAP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                DataSet dataSet = new DataSet();
+                dataSet.ReadXml(Server.MapPath("~/employees.data.xml"));
+                grdView.DataSource = dataSet;
+                grdView.DataBind();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(Server.MapPath("~/employees.data.xml"));
+            DataSet dataSet = new DataSet();
+            dataSet.ReadXml(Server.MapPath("~/employees.data.xml"));
+            int startValue = 1450 + dataSet.Tables[0].Rows.Count;
+            foreach (DataRow dataRow in dataSet.Tables[0].Rows)
+            {
+                XmlElement parentelement = xmldoc.CreateElement("employee");
+
+                XmlElement empid = xmldoc.CreateElement("EmployeeId");
+                XmlElement name = xmldoc.CreateElement("ReportsTo");
+                XmlElement designation = xmldoc.CreateElement("Name");
+                XmlElement city = xmldoc.CreateElement("Job");
+                XmlElement country = xmldoc.CreateElement("Phone");
+                XmlElement email = xmldoc.CreateElement("Email");
+                XmlElement orgUnit = xmldoc.CreateElement("OrgUnit");
+                XmlElement salary = xmldoc.CreateElement("Salary");
+                XmlElement gender = xmldoc.CreateElement("Gender");
+                XmlElement maritalStatus = xmldoc.CreateElement("MaritalStatus");
+                XmlElement employeeType = xmldoc.CreateElement("EmployeeType");
+                XmlElement employeeStatus = xmldoc.CreateElement("EmployeeStatus");
+                empid.InnerText = startValue.ToString();
+                name.InnerText = dataRow[1].ToString();
+                designation.InnerText = dataRow[2].ToString();
+                city.InnerText = dataRow[3].ToString();
+                country.InnerText = dataRow[4].ToString();
+                orgUnit.InnerText = dataRow[5].ToString();
+                salary.InnerText = dataRow[6].ToString();
+                gender.InnerText = dataRow[7].ToString();
+                maritalStatus.InnerText = dataRow[8].ToString();
+                employeeType.InnerText = dataRow[9].ToString();
+                employeeStatus.InnerText = dataRow[10].ToString();
+                parentelement.AppendChild(empid);
+                parentelement.AppendChild(name);
+                parentelement.AppendChild(designation);
+                parentelement.AppendChild(city);
+                parentelement.AppendChild(country);
+                parentelement.AppendChild(orgUnit);
+                parentelement.AppendChild(salary);
+                parentelement.AppendChild(gender);
+                parentelement.AppendChild(maritalStatus);
+                parentelement.AppendChild(employeeType);
+                parentelement.AppendChild(employeeStatus);
+
+                xmldoc.DocumentElement.AppendChild(parentelement);
+                startValue++;
+            }
+            xmldoc.Save(Server.MapPath("~/employees.data.xml"));
+        }
+
+        protected void grdView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            grdView.PageIndex = e.NewPageIndex;
+        }
+    }
+}
